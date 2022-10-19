fb560aa61b4613faae8cedb302b2dea765ee885d
Nathan Gaede <n.d.gaede@gmail.com>
2022-10-18 21:08:37 -0500

    Changed PORT number to avoid conflicts in PORT 5000. Added SQL code to database.sql. Created /public/images folder and imported mercury.png.

--- database.sql
+++ database.sql
@@ -8,3 +8,27 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "sighting" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id"  INT NOT NULL,
+	"planet_id" INT NOT NULL, 
+	"sighting_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+	"notes" VARCHAR(1000)
+	);
+
+CREATE TABLE "planet" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(255),
+	"description" VARCHAR(255),
+	"known_moons" INT,
+	"distance_from_sun" INT,
+	"planet_opposition_dates" DATE,
+	"visible_without_telescope" BOOLEAN,
+	"travel_time_driving_years" INT,
+	"travel_time_speed_of_light_minutes" INT,
+	"planet_image" VARCHAR(255)
+	 );
+
+INSERT INTO "planet" ("name", "description", "known_moons", "distance_from_sun", "visible_without_telescope", "travel_time_driving_years", "travel_time_speed_of_light_minutes", "planet_image")
+VALUES ('Mercury', 'Terrestrial', 0, 57900000, TRUE, 87, 8, '/public/images/mercury.png');
