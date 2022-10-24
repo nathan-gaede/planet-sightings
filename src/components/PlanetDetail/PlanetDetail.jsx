e65b7fe103bd13227fa3004a3bf96a3e683aa7c4
Nathan Gaede <n.d.gaede@gmail.com>
2022-10-24 14:37:42 -0500

    Removed JSON stringify from PlanetDetail return and replaced it with viewable elements populated from the  table.

--- src/components/PlanetDetail/PlanetDetail.jsx
+++ src/components/PlanetDetail/PlanetDetail.jsx
@@ -1,9 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
+import { actionChannel } from '@redux-saga/core/effects';
 
 function PlanetDetail() {
-    const planet = useSelector(store => store.planet.planetToDisplay);
+    const planet = useSelector(store => store.planet.selectedPlanet);
     const { id } = useParams();
     const dispatch = useDispatch();
 
@@ -12,8 +13,15 @@ function PlanetDetail() {
     }, [id])
     return (
         <div>
-            <h2>{id}</h2>
-           {JSON.stringify(planet.data)}
+            <h2>{planet.name}</h2>
+            <h2>Planet Type: {planet.description}</h2>
+            <img src={planet.planet_image}/>
+            <h2>Kilometers from Sun: {planet.distance_from_sun}</h2>
+            <h2>Known Moons: {planet.known_moons}</h2>
+            <h2>Visible without Telescope: {planet.visible_without_telescope}</h2>
+            <h2>Minutes from Earth at Speed of Light: {planet.travel_time_speed_of_light_minutes}</h2>
+            <h2>Time Required to Drive in Years: {planet.travel_time_driving_years}</h2>
+           
             
             
         </div>
