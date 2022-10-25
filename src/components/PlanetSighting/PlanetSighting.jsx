2cb9e122d31cecbc04c6f841c1f2c78f7791a315
Nathan Gaede <n.d.gaede@gmail.com>
2022-10-24 21:26:39 -0500

    Added background image to Planet Sighting component.

--- src/components/PlanetSighting/PlanetSighting.jsx
+++ src/components/PlanetSighting/PlanetSighting.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
+import './PlanetSighting.css';
 
 function PlanetSighting() {
     console.log('in Sighting');
